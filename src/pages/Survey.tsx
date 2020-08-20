7b48c0653f521475d1eb463fb0bde54fe8956874
ihlee <leeinhwan7@dgist.ac.kr>
2020-08-21 00:32:02 +0900

    wip

---
+++ src/pages/Survey.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { IonHeader, IonPage, IonTitle, IonToolbar } from "@ionic/react";
+
+const Survey: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>survey</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+    </IonPage>
+  );
+};
+
+export default Survey;
