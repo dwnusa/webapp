7e94af521a42dcbe8d73dbbeafbba3b1a811cc66
이인환 <ihlee@crscube.co.kr>
2020-08-21 09:20:13 +0900

    Revert "wip"
    This reverts commit 7b48c0653f521475d1eb463fb0bde54fe8956874.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { createContext } from "react";
+import React from "react";
 import { Redirect, Route } from "react-router-dom";
 import {
   IonApp,
@@ -34,58 +34,33 @@ import "@ionic/react/css/display.css";
 /* Theme variables */
 import "./theme/variables.css";
 
-import history from "./history";
-import Survey from "./pages/Survey";
-
-export const Context = createContext({ asdfa: true });
-
-const App: React.FC = () => {
-  const asdfa = /\/survey/.test(window.location.href);
-  const store = {
-    asdfa: true,
-  };
-  return (
-    <Context.Provider value={store}>
-      <IonApp>
-        <IonReactRouter history={history}>
-          <IonTabs>
-            <IonRouterOutlet>
-              <Route path="/tab1" component={MainTab} exact={true} />
-              <Route path="/tab2" component={CustomizeTab} exact={true} />
-              <Route path="/tab3" component={RecordTab} />
-              <Route path="/survey" component={Survey} />
-              <Route
-                path="/"
-                render={() => <Redirect to="/tab1" />}
-                exact={true}
-              />
-            </IonRouterOutlet>
-            <IonTabBar slot="bottom">
-              {!asdfa && (
-                <IonTabButton tab="tab1" href="/tab1">
-                  <IonIcon icon={homeSharp} />
-                  <IonLabel>메인화면</IonLabel>
-                </IonTabButton>
-              )}
-              {!asdfa && (
-                <IonTabButton tab="tab2" href="/tab2">
-                  <IonIcon icon={homeSharp} />
-                  <IonLabel>커스터마이징</IonLabel>
-                </IonTabButton>
-              )}
-              {!asdfa && (
-                <IonTabButton tab="tab3" href="/tab3">
-                  <IonIcon icon={homeSharp} />
-                  <IonLabel>수면기록</IonLabel>
-                </IonTabButton>
-              )}
-              {asdfa && <IonTabButton href="/tab3">다음</IonTabButton>}
-            </IonTabBar>
-          </IonTabs>
-        </IonReactRouter>
-      </IonApp>
-    </Context.Provider>
-  );
-};
+const App: React.FC = () => (
+  <IonApp>
+    <IonReactRouter>
+      <IonTabs>
+        <IonRouterOutlet>
+          <Route path="/tab1" component={MainTab} exact={true} />
+          <Route path="/tab2" component={CustomizeTab} exact={true} />
+          <Route path="/tab3" component={RecordTab} />
+          <Route path="/" render={() => <Redirect to="/tab1" />} exact={true} />
+        </IonRouterOutlet>
+        <IonTabBar slot="bottom">
+          <IonTabButton tab="tab1" href="/tab1">
+            <IonIcon icon={homeSharp} />
+            <IonLabel>메인화면</IonLabel>
+          </IonTabButton>
+          <IonTabButton tab="tab2" href="/tab2">
+            <IonIcon icon={homeSharp} />
+            <IonLabel>커스터마이징</IonLabel>
+          </IonTabButton>
+          <IonTabButton tab="tab3" href="/tab3">
+            <IonIcon icon={homeSharp} />
+            <IonLabel>수면기록</IonLabel>
+          </IonTabButton>
+        </IonTabBar>
+      </IonTabs>
+    </IonReactRouter>
+  </IonApp>
+);
 
 export default App;
--- src/pages/MainTab.tsx
+++ src/pages/MainTab.tsx
@@ -1,40 +1,23 @@
 import React from "react";
-import { IonContent, IonPage, IonAlert } from "@ionic/react";
+import {
+  IonContent,
+  IonPage,
+  IonAlert,
+} from "@ionic/react";
 import MainContainer from "../components/containers/MainContainer";
 import SlideContainer from "../components/containers/SlideContainer";
 import GridContainer from "../components/containers/GridContainer";
-import history from "../history";
-import { Context } from "../App";
 
 const MainTab: React.FC = () => {
   return (
     <IonPage>
       <IonContent>
-        <Context.Consumer>
-          {(store) => {
-            console.log(store);
-            return <div>{store.asdfa}</div>;
-          }}
-        </Context.Consumer>
         <IonAlert
           isOpen={true}
-          cssClass="my-custom-class"
-          header={"Info"}
-          message={
-            "나에게 맞는 수면정보를 위해 몇 가지 정보를 알려주세요! (예상 소요시간 3분)"
-          }
-          buttons={[
-            "괜찮아요",
-            {
-              text: "네, 좋아요",
-              role: "ok",
-              cssClass: "primary",
-              handler: (blah) => {
-                history.push("/survey");
-                console.log("Confirm Cancel: blah");
-              },
-            },
-          ]}
+          cssClass='my-custom-class'
+          header={'Info'}
+          message={'나에게 맞는 수면정보를 위해 몇 가지 정보를 알려주세요! (예상 소요시간 3분)'}
+          buttons={['괜찮아요', '네, 좋아요']}
         />
         <MainContainer name="김홍도" />
         <SlideContainer title="오늘의 추천 수면"></SlideContainer>
