d53ec98e1d5e764cb743cf84d0c10dc3057a2ce6
inhwan lee <leeinhwan7@dgist.ac.kr>
2020-08-26 23:40:27 +0900

    fix style

--- src/App.tsx
+++ src/App.tsx
@@ -34,6 +34,8 @@ import "@ionic/react/css/display.css";
 /* Theme variables */
 import "./theme/variables.css";
 
+import "./style.css";
+
 const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
