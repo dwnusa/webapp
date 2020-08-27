7cab1eb04fdc7a1f9199cee60694b527d3ee7456
inhwan lee <leeinhwan7@dgist.ac.kr>
2020-08-27 22:18:14 +0900

    img

--- src/components/containers/MainContainer.tsx
+++ src/components/containers/MainContainer.tsx
@@ -7,10 +7,8 @@ interface ContainerProps {
 
 const MainContainer: React.FC<ContainerProps> = ({ name }) => {
   return (
-    <div className="container">
-      <p>
-        안녕하세요,
-      </p>
+    <div className="main container">
+      <p>안녕하세요,</p>
       <p>
         <strong>{name}</strong> 님
       </p>
