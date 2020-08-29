e2b7126c502b410378039479765580f78e62f41f
inhwan lee <leeinhwan7@dgist.ac.kr>
2020-08-29 20:36:48 +0900

    설문

--- src/components/survey/Survey.tsx
+++ src/components/survey/Survey.tsx
@@ -8,64 +8,81 @@ import {
   IonListHeader,
   IonLabel,
   IonRadio,
+  IonAlert,
 } from "@ionic/react";
 import { close } from "ionicons/icons";
 import "./Survey.css";
+import surveyArray from "./survey.json";
 
 interface SurveyProps {
   setShowModal: (trigger: boolean) => void;
 }
 
 const Survey: React.FC<SurveyProps> = ({ setShowModal }) => {
-  const [selected, setSelected] = useState<number>(1);
+  const [currentNum, setCurrentNum] = useState<number>(0);
+  const [surveyResult, setSurveyResult] = useState<number[]>([]);
+  const [currentResult, setCurrentResult] = useState<number>();
+  const [showAlert, setShowAlert] = useState<boolean>(false);
 
   return (
     <Fragment>
-      <IonFab vertical="top" horizontal="end" slot="fixed">
-        <IonIcon icon={close} onClick={() => setShowModal(false)} />
-      </IonFab>
-
-      <IonRadioGroup
-        className="survey-group"
-        value={selected}
-        onIonChange={(e) => setSelected(e.detail.value)}
+      <div className="survey-page-wrapper">
+        <IonFab vertical="top" horizontal="end" slot="fixed">
+          <IonIcon icon={close} onClick={() => setShowModal(false)} />
+        </IonFab>
+        <IonAlert
+          isOpen={showAlert}
+          onDidDismiss={() => {
+            setShowModal(false);
+            setShowAlert(false);
+          }}
+          message={"제출 되었습니다."}
+          buttons={["OK"]}
+        />
+        <div className="survey-wrapper">
+          <IonRadioGroup
+            className="survey-group"
+            value={currentResult}
+            onIonChange={(e) => {
+              const result = surveyResult;
+              const changedValue = e.detail.value;
+              if (!isNaN(changedValue)) {
+                result[currentNum] = e.detail.value;
+                setSurveyResult(result);
+              }
+            }}
+          >
+            <IonListHeader>
+              <IonLabel>
+                {currentNum + 1}. {surveyArray[currentNum].title}
+              </IonLabel>
+            </IonListHeader>
+            {surveyArray[currentNum].questions.map((question, i) => {
+              return (
+                <IonItem key={i}>
+                  <IonLabel className="survey-question">{question}</IonLabel>
+                  <IonRadio slot="end" value={i} />
+                </IonItem>
+              );
+            })}
+          </IonRadioGroup>
+        </div>
+      </div>
+      <IonButton
+        expand="full"
+        size="large"
+        onClick={() => {
+          if (isNaN(surveyResult[currentNum])) {
+            return;
+          }
+          if (currentNum < surveyArray.length - 1) {
+            setCurrentNum(currentNum + 1);
+          } else {
+            setShowAlert(true);
+          }
+        }}
       >
-        <IonListHeader>
-          <IonLabel>얼마나 오랫동안 수면 장애가 있었나요?</IonLabel>
-        </IonListHeader>
-
-        <IonItem>
-          <IonLabel>문제 없음</IonLabel>
-          <IonRadio slot="end" value={1} />
-        </IonItem>
-
-        <IonItem>
-          <IonLabel>1주 이내</IonLabel>
-          <IonRadio slot="end" value={2} />
-        </IonItem>
-
-        <IonItem>
-          <IonLabel>1달 이내</IonLabel>
-          <IonRadio slot="end" value={3} />
-        </IonItem>
-
-        <IonItem>
-          <IonLabel>3개월 이내</IonLabel>
-          <IonRadio slot="end" value={4} />
-        </IonItem>
-
-        <IonItem>
-          <IonLabel>1년 이내</IonLabel>
-          <IonRadio slot="end" value={5} />
-        </IonItem>
-
-        <IonItem>
-          <IonLabel>1년 이상</IonLabel>
-          <IonRadio slot="end" value={6} />
-        </IonItem>
-      </IonRadioGroup>
-      <IonButton expand="full" size="large">
-        다음
+        {currentNum < surveyArray.length - 1 ? "다음" : "제출"}
       </IonButton>
     </Fragment>
   );
