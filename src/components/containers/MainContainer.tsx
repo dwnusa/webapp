6bbdd956b97d79aadd6b881083aeabecdf9ede56
inhwan lee <leeinhwan7@dgist.ac.kr>
2020-08-29 15:34:32 +0900

    img update

--- src/components/containers/MainContainer.tsx
+++ src/components/containers/MainContainer.tsx
@@ -6,8 +6,9 @@ interface ContainerProps {
 }
 
 const MainContainer: React.FC<ContainerProps> = ({ name }) => {
+  const random = Math.floor(Math.random() * 3);
   return (
-    <div className="main container">
+    <div className={`main container main-img${random}`}>
       <p>안녕하세요,</p>
       <p>
         <strong>{name}</strong> 님
