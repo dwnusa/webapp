281ea7e3c42911093820682b3091c8f8db78c639
inhwan lee <leeinhwan7@dgist.ac.kr>
2020-09-04 21:06:36 +0900

    설문 조사

--- src/pages/tab1/MainTab.tsx
+++ src/pages/tab1/MainTab.tsx
@@ -1,5 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { IonContent, IonPage, IonModal } from "@ionic/react";
+import {
+  IonContent,
+  IonPage,
+  IonModal,
+  IonButton,
+  IonIcon,
+} from "@ionic/react";
 import MainContainer from "components/containers/MainContainer";
 import SlideContainer from "components/containers/SlideContainer";
 import SurveyAlert from "components/tab1/alerts/SurveyAlert";
@@ -7,12 +13,13 @@ import Survey from "components/tab1/survey/Survey";
 import { useParams } from "react-router-dom";
 import { get } from "services";
 import { Content, httpStatus } from "types";
+import { pencilSharp } from "ionicons/icons";
 
 interface MainTabProps {
-  updateUserId: (id:number)=>void;
+  updateUserId: (id: number) => void;
 }
 
-const MainTab: React.FC<MainTabProps> = ({updateUserId}) => {
+const MainTab: React.FC<MainTabProps> = ({ updateUserId }) => {
   const [showModal, setShowModal] = useState(false);
   const [username, setUsername] = useState("익명");
   const [qactive, setQactive] = useState(false);
@@ -40,7 +47,6 @@ const MainTab: React.FC<MainTabProps> = ({updateUserId}) => {
     });
     setloading(true);
     updateUserId(id);
-
   }, [userId, updateUserId]);
   return (
     <IonPage>
@@ -51,11 +57,52 @@ const MainTab: React.FC<MainTabProps> = ({updateUserId}) => {
         {!qactive && !loading && (
           <SurveyAlert setShowModal={setShowModal}></SurveyAlert>
         )}
-        <MainContainer name={username} />
-        {!loadingContents && <SlideContainer title="오늘의 추천" contents={contentsState} recommend={[5, 6, 7, 3]}></SlideContainer> }
-        {!loadingContents && <SlideContainer title="배우기" contents={contentsState} recommend={[0, 1, 2]}></SlideContainer> }
-        {!loadingContents && <SlideContainer title="따라하기" contents={contentsState} recommend={[8, 9, 3]}></SlideContainer> }
-        {!loadingContents && <SlideContainer title="소리" contents={contentsState} recommend={[4, 5, 6, 7]}></SlideContainer> }
+        {qactive && <MainContainer name={username} />}
+        {!qactive && (
+          <div className="container-survey">
+            <div>나에게 맞는 수면정보를 위해 </div>
+            <div>설문조사를 진행해요!</div>
+            <div className="sub">(예상 소요시간 3분)</div>
+            <div
+              className="action"
+              onClick={() => {
+                setShowModal(true);
+              }}
+            >
+              <IonButton>
+                설문 조사하기 <IonIcon icon={pencilSharp} />
+              </IonButton>
+            </div>
+          </div>
+        )}
+        {!loadingContents && (
+          <SlideContainer
+            title="오늘의 추천"
+            contents={contentsState}
+            recommend={[5, 6, 7, 3]}
+          ></SlideContainer>
+        )}
+        {!loadingContents && (
+          <SlideContainer
+            title="배우기"
+            contents={contentsState}
+            recommend={[0, 1, 2]}
+          ></SlideContainer>
+        )}
+        {!loadingContents && (
+          <SlideContainer
+            title="따라하기"
+            contents={contentsState}
+            recommend={[8, 9, 3]}
+          ></SlideContainer>
+        )}
+        {!loadingContents && (
+          <SlideContainer
+            title="소리"
+            contents={contentsState}
+            recommend={[4, 5, 6, 7]}
+          ></SlideContainer>
+        )}
       </IonContent>
     </IonPage>
   );
